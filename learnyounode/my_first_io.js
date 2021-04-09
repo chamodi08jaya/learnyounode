14aab604a434cac36908d2fb5a30dbecebb23ef0
chamodi08jaya <mailtochamodij@gmail.com>
2021-04-09 17:02:22 +0530

    learnyounode

--- learnyounode/my_first_io.js
+++ learnyounode/my_first_io.js
@@ -1,6 +1,5 @@
-'use strict'
 const fs = require('fs')
 
 const contents = fs.readFileSync(process.argv[2])
-const lines = contents.toString().split('\n').length - 1
+const lines = contents.toString().split('\n')
 console.log(lines)
---
+++ learnyounodeWork/my-first-async-io.js
@@ -0,0 +1,11 @@
+const fs = require('fs')
+const file = process.argv[2]
+
+fs.readFile(file, function callback(err, data) {
+  if (err) {
+    return console.log(err)
+  }
+ 
+  const lines = data.toString().split('\n').length - 1
+  console.log(lines)
+})
